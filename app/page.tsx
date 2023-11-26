4633eabaf3b67f0744470b9cb8bd672345acba59
Fauzi Alhari <fauzialhari@gmail.com>
2023-11-26 12:19:23 +0700

    Add first slide bg-video and content

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,23 @@ export default async function Home() {
   return (
     <>
       <section className="section">
+        <div className="h-screen relative before:content-['*'] before:absolute before:w-full before:h-full before:bg-white before:opacity-25">
+          <video data-autoplay loop muted className="h-full object-cover">
+            <source
+              src="https://www.w3schools.com/html/mov_bbb.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <article className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center prose">
+            <h1>This is Title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              culpa veniam molestiae corrupti accusantium excepturi, voluptate
+              quos. Similique eius, nihil deleniti qui quisquam doloribus
+              aperiam iure perspiciatis animi. Ducimus, aliquam.
+            </p>
+          </article>
+        </div>
       </section>
       <div className="section">{JSON.stringify(data)}</div>
     </>
